2c0fdcbfecb8e4687e27b0867a0fc49a65ba20e1
tod619 <trevorodwyer619@hotmail.com>
2024-01-05 20:45:12 +0000

    create get method in main.js file

--- hash_table/main.js
+++ hash_table/main.js
@@ -22,10 +22,28 @@ class HashTable {
             this.data[address].push([key, value])
         }
     }
+
+    get(key) {
+        let address = this._hash(key)
+        const currentBucket = this.data[address]
+
+        if(currentBucket) {
+
+            for(let i = 0; i < currentBucket.length; i++) {
+                if(currentBucket[i][0] === key) {
+                    return currentBucket[i][1]
+                }
+            }
+
+        }
+
+        return undefined
+        
+    }
 }
 
 
 // Test Hash Table
 const myHashTable = new HashTable(50)
 myHashTable.set('grapes', 10000)
-//myHashTable.get('grapes')
+myHashTable.get('grapes')
