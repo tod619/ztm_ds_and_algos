fb4e9c80c25b55f5329ce5c6eeb67fb4f018c2f3
tod619 <trevorodwyer619@hotmail.com>
2024-01-05 20:13:05 +0000

    Create hash table template in main.js file

--- hash_table/main.js
+++ hash_table/main.js
@@ -1,1 +1,21 @@
 // Implementation of a Hash Table
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size)
+    }
+
+    _hash(key) {
+        let hash = 0
+        for(let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        }
+
+        return hash 
+    }
+}
+
+
+// Test Hash Table
+const myHashTable = new HashTable(50)
+myHashTable.set('grapes', 10000)
+myHashTable.get('grapes')
