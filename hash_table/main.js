932c855c734cf8e2751ab51917a4ee8ea1a13187
tod619 <trevorodwyer619@hotmail.com>
2024-01-05 20:31:33 +0000

    Create set method in main.js

--- hash_table/main.js
+++ hash_table/main.js
@@ -12,10 +12,20 @@ class HashTable {
 
         return hash 
     }
+
+    set(key, value) {
+        let address = this._hash(key)
+        if(!this.data[address]) {
+            this.data[address] = []
+            this.data[address].push([key, value])
+        } else {
+            this.data[address].push([key, value])
+        }
+    }
 }
 
 
 // Test Hash Table
 const myHashTable = new HashTable(50)
 myHashTable.set('grapes', 10000)
-myHashTable.get('grapes')
+//myHashTable.get('grapes')
