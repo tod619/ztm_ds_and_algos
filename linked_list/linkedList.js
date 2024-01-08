9e01527b9fa5690690c02d79b6ed9b4dd914ed3e
tod619 <trevorodwyer619@hotmail.com>
2024-01-08 22:04:59 +0000

    Create insert method in linkedList.js file

--- linked_list/linkedList.js
+++ linked_list/linkedList.js
@@ -45,9 +45,41 @@ class LinkedList {
 
     }
 
-    insert(index, value) {
-        const newNode = new Node(value)
+    traverseToIndex(index) {
+        let counter = 0
+
         let currentNode = this.head
+
+        while (counter !== index) {
+            currentNode = currentNode.next
+            counter++
+        }
+
+        return currentNode
+    }
+
+    insert(index, value) {
+
+        // Check params
+        if(index >= this.length) {
+            return this.append(value)
+        }
+
+        if(index === 0) {
+            return this.prepend(value)
+        }
+        
+
+        const newNode = new Node(value)
+        
+        const leader = this.traverseToIndex(index - 1)
+        const holdingPointer = leader.next
+
+        leader.next = newNode
+        newNode.next = holdingPointer
+        this.length++
+
+        
 
         
     }
@@ -57,4 +89,9 @@ const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
+console.log(myLinkedList.printList())
+myLinkedList.insert(2, 99)
+myLinkedList.insert(20, 88)
+console.log(myLinkedList.printList())
+myLinkedList.insert(0, 15)
 console.log(myLinkedList.printList())
