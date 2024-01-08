71783db7cad5b0ed4385774c56d63dcfc25de439
tod619 <trevorodwyer619@hotmail.com>
2024-01-08 21:21:52 +0000

    Create Node class for linked list

--- linked_list/linkedList.js
+++ linked_list/linkedList.js
@@ -1,31 +1,31 @@
 // An implementation of a linked list in JavaScript
 
+class Node {
+
+    constructor(value) {
+        this.value = value
+        this.next = null 
+    }
+
+}
+
 class LinkedList {
     constructor(value) {
 
-        this.head = {
-            value: value,
-            next: null
-        }
+        this.head = new Node(value)
         this.tail = this.head
         this.length = 1
     }
 
     append(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value)
         this.tail.next = newNode
         this.tail = newNode
         this.length++
     }
 
     prepend(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value)
 
         newNode.next = this.head
         this.head = newNode
