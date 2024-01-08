1f8b02305a8a42f72e0dcd4d97f01647bc6c79b9
tod619 <trevorodwyer619@hotmail.com>
2024-01-08 21:12:26 +0000

    Create prepend method for Linked List

--- linked_list/linkedList.js
+++ linked_list/linkedList.js
@@ -20,9 +20,21 @@ class LinkedList {
         this.tail = newNode
         this.length++
     }
+
+    prepend(value) {
+        const newNode = {
+            value: value,
+            next: null
+        }
+
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+    }
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
+myLinkedList.prepend(1)
 console.log(myLinkedList)
