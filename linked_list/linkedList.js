2c101832686f5e345956765956098e86406ddd97
tod619 <trevorodwyer619@hotmail.com>
2024-01-08 21:03:35 +0000

    Create append method for linkedList in linkedList.js file

--- linked_list/linkedList.js
+++ linked_list/linkedList.js
@@ -10,7 +10,19 @@ class LinkedList {
         this.tail = this.head
         this.length = 1
     }
+
+    append(value) {
+        const newNode = {
+            value: value,
+            next: null
+        }
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++
+    }
 }
 
 const myLinkedList = new LinkedList(10)
+myLinkedList.append(5)
+myLinkedList.append(16)
 console.log(myLinkedList)
