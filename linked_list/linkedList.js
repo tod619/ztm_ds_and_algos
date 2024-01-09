14be053f193301a72f35c859af352cd115d0df68
tod619 <trevorodwyer619@hotmail.com>
2024-01-09 22:57:34 +0000

    Create reverse method in linkedList.js file

--- linked_list/linkedList.js
+++ linked_list/linkedList.js
@@ -87,6 +87,27 @@ class LinkedList {
         this.length--
 
     }
+
+    reverse() {
+        if(!this.head.next) {
+            return this.head
+        }
+
+        let first = this.head
+        this.tail = this.head 
+        let second = first.next 
+
+        while(second) {
+            const temp = second.next
+            second.next = first
+            first = second
+            second = temp 
+        }
+
+        this.head.next = null 
+        this.head = first 
+            
+    }
 }
 
 const myLinkedList = new LinkedList(10)
@@ -100,4 +121,6 @@ console.log(myLinkedList.printList())
 myLinkedList.insert(0, 15)
 console.log(myLinkedList.printList())
 myLinkedList.delete(2)
+console.log(myLinkedList.printList())
+myLinkedList.reverse()
 console.log(myLinkedList.printList())
