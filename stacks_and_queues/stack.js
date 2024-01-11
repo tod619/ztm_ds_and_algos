84c90a78ebce4155ebcdb01aa93790755af32258
tod619 <trevorodwyer619@hotmail.com>
2024-01-11 22:00:45 +0000

    Create push, peek, pop methods for stack

--- stacks_and_queues/stack.js
+++ stacks_and_queues/stack.js
@@ -12,4 +12,47 @@ class Stack {
         this.bottom = null
         this.length = 0
     }
+
+    peek() {
+        return this.top 
+    }
+
+    push(value) {
+        const newNode = new Node(value)
+
+        if(this.length === 0) {
+            this.top = newNode
+            this.bottom = newNode
+        } else {
+            const holdingPointer = this.top
+            this.top = newNode
+            this.top.next = holdingPointer
+
+        }
+
+        this.length++
+        return this
+    }
+
+    pop() {
+        if(!this.top) {
+            return null
+        }
+
+        const holdingPointer = this.top
+        this.top = this.top.next
+        this.length--
+        
+        return holdingPointer
+    }
 }
+
+const myStack = new Stack()
+myStack.push("Discord")
+myStack.push("YouTube")
+myStack.push("Facebook")
+console.log(myStack.peek())
+firstItem = myStack.pop()
+console.log(`1 POP Top Stack Item: ${firstItem.value}, ${firstItem.length}`)
+secondItem = myStack.pop()
+console.log(`2 POP Top Stack Item: ${firstItem.value}, ${firstItem.length}`)
