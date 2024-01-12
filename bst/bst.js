c0e1059dfec0d9cc0683d931dd75d68069b370c1
tod619 <trevorodwyer619@hotmail.com>
2024-01-12 22:33:46 +0000

    Create bst.js folder + file + Node code

---
+++ bst/bst.js
@@ -0,0 +1,14 @@
+// An implementation of a binary search tree
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+}
