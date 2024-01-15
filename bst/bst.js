7a66ce6a8d1dfa1bcf9b080c19d18e5ad1e90923
tod619 <trevorodwyer619@hotmail.com>
2024-01-15 20:20:57 +0000

    create insert function in bst.js file

--- bst/bst.js
+++ bst/bst.js
@@ -12,9 +12,39 @@ class BinarySearchTree {
         this.root = null
     }
 
-    
-}
+    insert(value) {
+        const newNode = new Node(value)
 
+        if(this.root === null) {
+            this.root = newNode 
+        }else {
+            let currentNode = this.root
+            while(true) {
+                if(value < currentNode.value) {
+                    // Go Left
+                    if(!currentNode.left) {
+                        currentNode.left = newNode
+                        return this 
+                    }
+
+                    currentNode = currentNode.left 
+                } else {
+                    // Go right
+                    if(!currentNode.right) {
+                        currentNode.right = newNode
+                        return this 
+                    }
+
+                    currentNode = currentNode.right 
+                }
+            }
+        }
+
+
+    }
+
+
+}
 
 function traverse(node) {
     const tree = {value: node.value}
@@ -22,3 +52,13 @@ function traverse(node) {
     tree.right = node.right === null ? null : traverse(node.right)
     return tree 
 }
+
+const tree = new BinarySearchTree()
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+console.log(JSON.stringify(traverse(tree.root)))
