771b448b838028b005ed66bb4c7338d46832676b
tod619 <trevorodwyer619@hotmail.com>
2024-01-16 18:54:43 +0000

    Create Edge + adjacent graphs in graph.js file

---
+++ graphs/graph.js
@@ -0,0 +1,5 @@
+// Implementation of an Edge List Graph
+const edgeGraph = [[0, 2], [2, 3], [2, 1], [1, 3] ]
+
+//Implementation of na Adjacent List
+const adjacentList = [[2], [2, 3], [0, 1, 3], [1, 2]]
