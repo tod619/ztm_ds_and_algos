f755666d3ad290c767fc18d9249122b0b1917c16
tod619 <trevorodwyer619@hotmail.com>
2024-01-16 19:03:24 +0000

    Create Adjacent Matrix graph in graph.js file

--- graphs/graph.js
+++ graphs/graph.js
@@ -3,3 +3,11 @@ const edgeGraph = [[0, 2], [2, 3], [2, 1], [1, 3] ]
 
 //Implementation of na Adjacent List
 const adjacentList = [[2], [2, 3], [0, 1, 3], [1, 2]]
+
+//Implementation of an Adjacent Matrix graph
+const adjacentMatrixGraph = [
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [1, 1, 0, 1],
+    [0, 1, 1, 0]
+]
