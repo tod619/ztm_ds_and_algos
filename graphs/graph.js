cce900846373650b88e0cb355bcf0a6bcde31caa
tod619 <trevorodwyer619@hotmail.com>
2024-01-16 19:07:34 +0000

    Add comment to graph.js file

--- graphs/graph.js
+++ graphs/graph.js
@@ -1,3 +1,11 @@
+// Graph representations of the following graph
+//          2 ----- 0
+//         / \
+//        /   \
+//       /     \
+//      1 ----- 3
+
+
 // Implementation of an Edge List Graph
 const edgeGraph = [[0, 2], [2, 3], [2, 1], [1, 3] ]
 
