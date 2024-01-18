94811f014945bd54557d9185849839b428ccd6b3
tod619 <trevorodwyer619@hotmail.com>
2024-01-18 19:39:53 +0000

    Create an implementation of selection sort

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -1,1 +1,25 @@
 // An Implementation of Selection Sort Algorithm
+function selectionSort(arr) {
+    const length = arr.length
+
+    for(let i = 0; i < length; i++) {
+        let min = i
+        let temp = arr[i]
+
+        for(let j = i+1; j < length; j++) {
+            if(arr[j] < arr[min]) {
+                min = j
+            }
+        }
+
+        arr[i] = arr[min]
+        arr[min] = temp
+    }
+
+    return arr 
+    
+}
+
+const numbers = [99, 40, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+selectionSort(numbers)
+console.log(numbers)
