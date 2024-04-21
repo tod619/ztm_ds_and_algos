b21bac26ced00e0e038104008ccc1c85a77ff1f3
tod619 <trevorodwyer619@hotmail.com>
2024-04-21 17:51:54 +0100

    add starter code in quick.js file

---
+++ sorting/quick.js
@@ -0,0 +1,17 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(array, left, right) {
+ 
+}
+
+function partition(array, pivot, left, right) {
+  
+}
+
+function swap(array, firstIndex, secondIndex) {
+  
+}
+
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
+console.log(numbers);
